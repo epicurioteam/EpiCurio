f37d98e0a02465849ac2bd1fe259c200aa8aa144
francismungai <epicurio2023@gmail.com>
2023-08-15 16:00:51 -0400

    fields rendering

--- client/src/components/Form/labItemForm.js
+++ client/src/components/Form/labItemForm.js
@@ -1,51 +1,65 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import { TextField, Button, Typography, Paper } from '@material-ui/core'; 
-import FileBase from 'react-file-base64';
-import { useDispatch } from 'react-redux';
-import useStyles from './styles';
-import { fetchCategoryFields } from '../../actions/labItem.js';
+import React, { useState, useEffect } from "react";
+import { Provider, useSelector } from "react-redux";
+import { TextField, Button, Typography, Paper, Grid } from "@material-ui/core";
+import FileBase from "react-file-base64";
+import { useDispatch } from "react-redux";
+import useStyles from "./styles";
+import { fetchCategoryFields } from "../../actions/labItem.js";
 
-import { updatePost, createPost } from '../../actions/posts'; // needs to look at actions
+import { updatePost, createPost } from "../../actions/posts";
+import Input from "../Auth/Input.js"; // needs to look at actions
+import { configureStore } from "@reduxjs/toolkit";
+import labItemReducer from "../../reducers/labItem";
 
+const LabItemFormWrapper = () => {
+  const store = configureStore({ reducer: labItemReducer });
+
+  return (
+    <Provider store={store}>
+      <LabItemForm />
+    </Provider>
+  );
+};
 
 const LabItemForm = () => {
-  const [category, setCategory] = useState('');
+  const [category, setCategory] = useState("");
   const [formData, setFormData] = useState({
-    name: '',
-    unit_quantity: '',
-    location: '',
-    shelf_life: '',
-    vendor: '',
-    description: '',
-    category: '', 
-    creator: ''
+    name: "",
+    unit_quantity: "",
+    location: "",
+    shelf_life: "",
+    vendor: "",
+    description: "",
+    category: "",
+    creator: "",
   });
 
   const classes = useStyles();
   const dispatch = useDispatch();
+  const fields = useSelector((itemFields) => itemFields);
 
   const clear = () => {
-    setFormData({ name: '',
-    unit_quantity: '',
-    location: '',
-    shelf_life: '',
-    vendor: '',
-    description: '',
-    category: '', 
-    creator: ''});
-  }
+    setFormData({
+      name: "",
+      unit_quantity: "",
+      location: "",
+      shelf_life: "",
+      vendor: "",
+      description: "",
+      category: "",
+      creator: "",
+    });
+  };
 
   const handleCategoryChange = (event) => {
     setCategory(event.target.value);
   };
-  
+
   useEffect(() => {
     if (category) {
       dispatch(fetchCategoryFields(category));
     }
   }, [category, dispatch]);
-  
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -57,37 +71,39 @@ const LabItemForm = () => {
 
   return (
     <form>
-      <div>
-        <label htmlFor="category">Category:</label>
-        <select id="category" name="category" value={category} onChange={handleCategoryChange}>
-          <option disabled>Select a category</option>
-          <option value="glassPlasticWare">glassPlasticWare</option>
-          <option value="electronics">electronics</option>
-          <option value="safetyEquipment">safetyEquipment</option>
-        </select>
-      </div>
-      <div>
-        <input
-          type="text"
-          name="name"
-          placeholder="Name"
-          value={formData.name}
-          onChange={handleInputChange}
-        />
-      </div>
-      <div>
-        <input
-          type="text"
-          name="description"
-          placeholder="Description"
-          value={formData.description}
-          onChange={handleInputChange}
-        />
-      </div>
+      <Grid container spacing={2}>
+        <div>
+          <label htmlFor="category">Category:</label>
+          <select
+            id="category"
+            name="category"
+            value={category}
+            onChange={handleCategoryChange}
+          >
+            <option disabled>Select a category</option>
+            <option value="glassPlasticWare">glassPlasticWare</option>
+            <option value="electronics">electronics</option>
+            <option value="safetyEquipment">safetyEquipment</option>
+          </select>
+        </div>
+
+        {/* render the fields returned by the category */}
+        {fields
+          .filter((field) => field !== "_id" && field !== "__v")
+          .map((field) => (
+            <div key={`${field}`}>
+              <Input
+                type="string"
+                name={`${field}`}
+                handleChange={handleInputChange}
+              />
+            </div>
+          ))}
+      </Grid>
       {/* {renderAdditionalFields()} */}
       <button type="submit">Submit</button>
     </form>
   );
 };
 
-export default LabItemForm;
+export default LabItemFormWrapper;
